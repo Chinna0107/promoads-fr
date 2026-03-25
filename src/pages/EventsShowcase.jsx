eac593e9812dd9fd8a5bd664d8ecc968fde36507
chinna0107 <kancharlahemanth89@gmail.com>
2026-03-25 08:50:59 +0530

    Production 2.5

--- src/pages/EventsShowcase.jsx
+++ src/pages/EventsShowcase.jsx
@@ -23,13 +23,13 @@ const EventsShowcase = () => {
     //   description: 'Solve puzzles and crack the code',
     //   link: 'https://codeathon-three.vercel.app/'
     // },
-    //  {
-    //   id: 4,
-    //   name: 'Crack The Code  first years',
-    //   image: 'https://res.cloudinary.com/dgyykbmt6/image/upload/v1772647786/4_p9eplw.png',
-    //   description: 'Solve puzzles and crack the code',
-    //   link: 'https://quiz-app-orcin-kappa.vercel.app/'
-    // }
+     {
+      id: 4,
+      name: 'Crack The Code  first years',
+      image: 'https://res.cloudinary.com/dgyykbmt6/image/upload/v1772647786/4_p9eplw.png',
+      description: 'Solve puzzles and crack the code',
+      link: 'https://quiz-app-orcin-kappa.vercel.app/'
+    }
   ];
 
   return (
